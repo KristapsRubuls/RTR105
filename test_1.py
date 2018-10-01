d2673b5b87410902db5ff7d9b7882059afd92fb7
user <kristapsr1999@gmail.com>
2018-10-01 17:46:46 +0300

    20181001_17_46

---
+++ test_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+#https://www.cplusplus.com/reference/cstdio/printf/
+
+a = 10
+b = 1.99
+c = 'A'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+
+a = 20
+b = 3.98
+c = 'A'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
+
+a = 40
+b = 7.6666666
+c = 'A'
+print("a = %d"%(a))
+print("b = %.4f"%(b))
+print("c = %s"%(c))
