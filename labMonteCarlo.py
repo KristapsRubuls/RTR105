c89436688d7f0bdcb9ec8f256a2ac6644136c513
user <kristapsr1999@gmail.com>
2018-12-19 19:51:37 +0200

    20181219

--- labMonteCarlo.py
+++ labMonteCarlo.py
@@ -1,40 +1,41 @@
 import sys
 sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
-from math import*
-from numpy import sinh, linspace, sqrt, random
+from numpy import*
 from matplotlib import pyplot as plt
+
+
+def f(x):
+    return sinh(sqrt(x))
+
+x = linspace(0, 10, 1000)
+y = f(x)
 
 N = 1000
 a = 0
-b = 5
-
-#pseido-gadiijuma skaitlju generatora grauds
-#random.seed(1)
-
-x = linspace(0, 5, 1000)
-y = sinh(sqrt(x))
+b = 10
+c = 12
 
 plt.grid()
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Funkcija un taas integraalis (laukums starp funkciju un x ass)')
-#nav jeegas ziimeet shaadi plt.plot(x,y)
+
 plt.plot(x,y)
 
-
 x1 = random.uniform(a,b,N)
+y1 = random.uniform(a,c,N)
 
-y1 = random.uniform(a,b,N)
-
-
-N1 = 0
+#N1 = 0
 for i in range(N):
-    if y1[i] < y[i]:
+    if y1[i] < f(x1[i]):
         plt.plot(x1[i],y1[i],'go')
-        N1 = N1 + 1
     else:
         plt.plot(x1[i],y1[i],'ro')
 
+print(x)
+print(y)
+print(x1)
+print(y1)
 #S_zinaamais = (b-a) * (b-a)
 #S_nezinaamais = 1. * S_zinaamais * N1 / N
 #print(S_nezinaamais)
--- labatvasinajumi.py
+++ labatvasinajumi.py
@@ -8,77 +8,51 @@ def f(x):
     return sinh(sqrt(x))
 
 
-x = linspace (0, 10, 20)
+x = linspace (0, 10, 100)
 
 y = f(x)
 
 
-#legend = []
-#print(legend)
 
-
-plt.axis([0, 10, 0, 10])
+legend = []
+#plt.axis([0, 10, 0, 10])
 plt.grid()
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("Funkcija $sinh(sqrt(x))$ un tās atvasinajumi")
+plt.title("Funkcija $sinh(sqrt(x))$ un tās atvasinājumi")
 plt.plot(x, y, "k")
 
-
-#legend.append("$sinh(sqrt(x))$ - default - viss ir savienots ar taisnam linijam")
-#print(legend)
-
+legend.append("$sinh(sqrt(x))$ - default - viss ir savienots ar taisnām linijam")
 
 plt.plot(x, y, "ro")
 
-
-#legend.append("$sinh(sqrt(x))$ - tikai dazi punkti")
-#print(legend)
-
+legend.append("$sinh(sqrt(x))$ - tikai daži punkti")
 
 N = len(x)
-deltax = x[1] - x[0]
-atvasinajums = []
 
-for i in range(N):
-    temp = (f(x[i] + deltax) - f(x[i]))/deltax
-    atvasinajums.append(temp)
+atvasinajumscaurmassivu = []
+for i in range(N-1):
+    temp = (y[i+1] - y[i]) / (x[i+1] - x[i])
+    atvasinajumscaurmassivu.append(temp)
 
-plt.plot(x, atvasinajums, "y")
-#legend.append("$sinh(sqrt(x))$ atvasinajums - viss ir savienots ar taisnam linijam")
-
-plt.plot(x, atvasinajums, "go")
-#legend.append("$sinh(sqrt(x))$ atvasinajums - dazi punkti")
-
-N = len(atvasinajums)
-deltax = atvasinajums[1] - atvasinajums[0]
-atvasinajums2 = []
-
-for i in range(N):
-    temp = (f(atvasinajums[i] + deltax) - f(atvasinajums[i]))/deltax
-    atvasinajums2.append(temp)
-
-plt.plot(x, atvasinajums2, "m")
-plt.plot(x, atvasinajums2, "bo")
+plt.plot(x[0:N-1], atvasinajumscaurmassivu, "m")
+legend. append("$sinh(sqrt(x))$ atvasinājums, izmantojot massīva vertības - viss ir savienots ar taisnām līnijām")
+plt.plot(x[0:N-1], atvasinajumscaurmassivu, "bo")
+legend. append("$sinh(sqrt(x))$ atvasinājums, izmantojot massīva vertības - daži punkti")
 
 
-#atvasinajumscaurmassivu = []
-#for i in range(N-1):
-    #temp = (y[i+1] - y[i]) / (x[i+1] - x[i])
-    #atvasinajumscaurmassivu.append(temp)
+atvasinajumscaurmassivu2 = []
+for i in range(N-2):
+    temp = (atvasinajumscaurmassivu[i+1] - atvasinajumscaurmassivu[i]) / (x[i+1] - x[i])
+    atvasinajumscaurmassivu2.append(temp)
+
+plt.plot(x[0:N-2], atvasinajumscaurmassivu2, "y")
+legend. append("$sinh(sqrt(x))$ otrās kārtas atvasinājums, izmantojot massīva vertības - viss ir savienots ar taisnām līnijām")
+plt.plot(x[0:N-2], atvasinajumscaurmassivu2, "go")
+legend. append("$sinh(sqrt(x))$ otrās kārtas atvasinājums, izmantojot massīva vertības - daži punkti")
 
 
-#plt.plot(x[0:N-1], atvasinajumscaurmassivu, "m")
-#legend. append("$sinh(sqrt(x))$ atvasinajums, izmantojot massiva vertibas - viss ir savienots ar taisnam linijam")
-#plt.plot(x[0:N-1], atvasinajumscaurmassivu, "bo")
-#legend. append("$sinh(sqrt(x))$ atvasinajums, izmantojot massiva vertibas - dazi punkti")
 
 
-#plt.plot(0.680, 0.620, "ch", markersize = 10)
-
-
-#print(plt.legend.__doc__)
-#plt.legend(legend, loc = "lower left")
-#plt.legend(legend, loc = 3, fancybox = True, framealpha = 0.5, facecolor = "orange")
-
+plt.legend(legend, loc = 3, fancybox = True, framealpha = 0.5, facecolor = "orange")
 plt.show()
